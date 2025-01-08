974870309fb18cace4bfb48295cc82ccb948a5ad
GoodByeDaylight <yuyuw.060303@gmail.com>
2025-01-08 09:03:36 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,26 +1,12 @@
-// ヘッダー画像の切り替え
-let currentIndex = 0;
 const images = [
-    'img/hai.jpg',
-    'img/kei.jpg',
-    'img/foto.jpg'
+    'img/hai.png',
+    'img/kei.png',
+    'img/foto.png'
 ];
-const headerImages = document.querySelectorAll('.header-image');
-
-// 画像を交代する関数
-function changeImage() {
-    // 現在の画像を非表示に
-    headerImages[currentIndex].classList.remove('active');
-    
-    // 次の画像を表示
+let currentIndex = 0;
+function changeBackground() {
     currentIndex = (currentIndex + 1) % images.length;
-    headerImages[currentIndex].style.backgroundImage = `url(${images[currentIndex]})`;
-    headerImages[currentIndex].classList.add('active');
+    document.body.style.backgroundImage = `url(${images[currentIndex]})`;
 }
-
-// 3秒ごとに画像を切り替え
-setInterval(changeImage, 3000);
-
-// 最初に表示する画像を設定
-headerImages[currentIndex].style.backgroundImage = `url(${images[currentIndex]})`;
-headerImages[currentIndex].classList.add('active');
+document.body.style.backgroundImage = `url(${images[currentIndex]})`;
+setInterval(changeBackground, 5000);
